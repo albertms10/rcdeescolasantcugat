43a550eabad456b0b64f8785c77707c942c7c07c
albertms10 <albertmasa2@gmail.com>
2021-02-09 23:05:24 +0100

    refactor(post-missatge): extract send_mail logic
    * rename contact-email-template.php file

---
+++ src/Utils/send-mail.php
@@ -0,0 +1,147 @@
+<?php
+
+use Arrilot\DotEnv\DotEnv;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+function error_header(string $location, Exception $e = null)
+{
+    $message = empty($e) ? 'no_exception_provided' : $e->errorMessage();
+    header("Location: $location?res=err&msg=$message");
+}
+
+function mailer_error(string $location, PHPMailer $mail, Exception $e = null): string
+{
+    error_header($location, $e ?? new Exception($mail->ErrorInfo));
+    return 'Mailer Error' . PHP_EOL . $mail->ErrorInfo;
+}
+
+if (empty(ROOT)) {
+    define('ROOT', $_SERVER['DOCUMENT_ROOT']);
+}
+
+require_once ROOT . '/../vendor/PHPMailer/PHPMailer.php';
+require_once ROOT . '/../vendor/PHPMailer/SMTP.php';
+require_once ROOT . '/../vendor/PHPMailer/Exception.php';
+require_once ROOT . '/../vendor/DotEnv/DotEnv.php';
+require_once ROOT . '/../vendor/DotEnv/Exceptions/MissingVariableException.php';
+
+require_once ROOT . '/../src/Controller/MissatgeController.php';
+
+function send_mail(
+    string $email,
+    string $name,
+    string $subject = '',
+    string $message = '',
+    array $bccs = [],
+    string $template = 'contact',
+    string $error_location = '',
+)
+{
+    DotEnv::load(ROOT . '/../.env.php');
+    DotEnv::setRequired(['MAILER_HOST', 'MAILER_USERNAME', 'MAILER_PASSWORD']);
+
+    setlocale(LC_TIME, 'ca_ES', 'Catalan_Spain', 'Catalan');
+    date_default_timezone_set('Europe/Madrid');
+
+    // SMTP needs accurate times, and the PHP time zone MUST be set
+    // This should be done in your php.ini, but this is how to do it if you don't have access to that
+    date_default_timezone_set('Europe/Madrid');
+
+    $mail = new PHPMailer(true);
+
+    $mail->CharSet = 'UTF-8';
+    $mail->Encoding = 'base64';
+    $mail->isSMTP();
+
+    $mail->SMTPOptions = [
+        'ssl' => [
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true,
+        ]
+    ];
+
+    // Enable SMTP debugging:
+    //   SMTP::DEBUG_OFF = off (for production use)
+    //   SMTP::DEBUG_CLIENT = client messages
+    //   SMTP::DEBUG_SERVER = client and server messages
+    $mail->SMTPDebug = SMTP::DEBUG_OFF;
+
+    $mail->Host = DotEnv::get('MAILER_HOST');
+    $mail->Port = DotEnv::get('MAILER_PORT');
+
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'tls';
+
+    $mail->Username = DotEnv::get('MAILER_USERNAME');
+    $mail->Password = DotEnv::get('MAILER_PASSWORD');
+
+    try {
+        $mail->setFrom('webmaster@rcdeescolasantcugat.com', 'RCDE Escola Sant Cugat');
+    } catch (Exception $e) {
+        echo mailer_error($error_location, $mail, $e);
+        return;
+    }
+
+    try {
+        $mail->addReplyTo($email, $name);
+    } catch (Exception $e) {
+        echo mailer_error($error_location, $mail, $e);
+        return;
+    }
+
+    try {
+        foreach ($bccs as $bcc) {
+            $mail->addBCC($bcc['address'], $bcc['name']);
+        }
+    } catch (Exception $e) {
+        echo mailer_error($error_location, $mail, $e);
+        return;
+    }
+
+    $mail->Subject = $subject;
+
+    $body = match ($template) {
+        'contact' => contact_email_template($email, $name, $message),
+    };
+
+    try {
+        $mail->msgHTML($body, __DIR__);
+    } catch (Exception $e) {
+        echo mailer_error($error_location, $mail, $e);
+        return;
+    }
+
+    $mail->AltBody = $message;
+
+    try {
+        if (!$mail->send()) {
+            echo mailer_error($error_location, $mail);
+            return;
+        }
+    } catch (Exception $e) {
+        echo mailer_error($error_location, $mail, $e);
+        return;
+    }
+}
+
+function contact_email_template(string $email, string $name, string $message): string
+{
+    $body = file_get_contents(ROOT . '/../src/View/contact-email-template.php');
+
+    $vars = [
+        'email' => $email,
+        'from' => $name,
+        'date' => date('Y-m-d H:i'),
+        'date-format' => strftime('%A, %e %B %Y · %H:%M'),
+        'message' => $message,
+    ];
+
+    foreach ($vars as $var => $value) {
+        $body = str_replace("{{$var}}", $value, $body);
+    }
+
+    return $body;
+}
--- web/api/post-missatge.php
+++ web/api/post-missatge.php
@@ -1,41 +1,19 @@
 <?php
 
-use Arrilot\DotEnv\DotEnv;
-use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
+define('ROOT', $_SERVER['DOCUMENT_ROOT']);
 
-function error_header(Exception $e = null)
-{
-    $message = empty($e) ? 'no_exception_provided' : $e->errorMessage();
-    header("Location: https://rcdeescolasantcugat.com/contacte/?res=err&msg=$message");
-}
+require_once ROOT . '/../src/Utils/send-mail.php';
+
+$location = 'https://rcdeescolasantcugat.com/contacte/';
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    error_header();
+    error_header($location);
     return;
 }
 
-function mailer_error(PHPMailer $mail, Exception $e = null): string
-{
-    error_header($e ?? new Exception($mail->ErrorInfo));
-    return 'Mailer Error' . PHP_EOL . $mail->ErrorInfo;
-}
-
-define('ROOT', $_SERVER['DOCUMENT_ROOT']);
-
-require_once ROOT . '/../vendor/PHPMailer/PHPMailer.php';
-require_once ROOT . '/../vendor/PHPMailer/SMTP.php';
-require_once ROOT . '/../vendor/PHPMailer/Exception.php';
-require_once ROOT . '/../vendor/DotEnv/DotEnv.php';
-require_once ROOT . '/../vendor/DotEnv/Exceptions/MissingVariableException.php';
-
-require_once ROOT . '/../src/Model/Missatge.php';
+require_once ROOT . '/../src/Controller/MissatgeController.php';
 
 session_start();
-
-DotEnv::load(ROOT . '/../.env.php');
-DotEnv::setRequired(['MAILER_HOST', 'MAILER_USERNAME', 'MAILER_PASSWORD']);
 
 $_SESSION['nom'] = $_POST['nom'] ?? '';
 $_SESSION['email'] = $_POST['email'] ?? '';
@@ -47,104 +25,24 @@ if (
     preg_match($url_pattern, $_POST['nom'])
     || preg_match($url_pattern, $_POST['missatge'])
 ) {
-    header('Location: https://rcdeescolasantcugat.com/contacte/?res=invalid&contains-link');
+    header("Location: $location?res=invalid&contains-link");
     return;
 }
 
-RCDE\Missatge::postMissatge($_POST['nom'], $_POST['email'], $_POST['missatge']);
+RCDE\MissatgeController::postMissatge($_POST['nom'], $_POST['email'], $_POST['missatge']);
 
-setlocale(LC_TIME, 'ca_ES', 'Catalan_Spain', 'Catalan');
-date_default_timezone_set('Europe/Madrid');
-
-// SMTP needs accurate times, and the PHP time zone MUST be set
-// This should be done in your php.ini, but this is how to do it if you don't have access to that
-date_default_timezone_set('Europe/Madrid');
-
-$mail = new PHPMailer(true);
-
-$mail->CharSet = 'UTF-8';
-$mail->Encoding = 'base64';
-$mail->isSMTP();
-
-$mail->SMTPOptions = [
-    'ssl' => [
-        'verify_peer' => false,
-        'verify_peer_name' => false,
-        'allow_self_signed' => true,
-    ]
-];
-
-// Enable SMTP debugging:
-//   SMTP::DEBUG_OFF = off (for production use)
-//   SMTP::DEBUG_CLIENT = client messages
-//   SMTP::DEBUG_SERVER = client and server messages
-$mail->SMTPDebug = SMTP::DEBUG_OFF;
-
-$mail->Host = DotEnv::get('MAILER_HOST');
-$mail->Port = DotEnv::get('MAILER_PORT');
-
-$mail->SMTPAuth = true;
-$mail->SMTPSecure = 'tls';
-
-$mail->Username = DotEnv::get('MAILER_USERNAME');
-$mail->Password = DotEnv::get('MAILER_PASSWORD');
-
-try {
-    $mail->setFrom('webmaster@rcdeescolasantcugat.com', 'RCDE Escola Sant Cugat');
-} catch (Exception $e) {
-    echo mailer_error($mail, $e);
-    return;
-}
-
-try {
-    $mail->addReplyTo($_POST['email'], $_POST['nom']);
-} catch (Exception $e) {
-    echo mailer_error($mail, $e);
-    return;
-}
-
-try {
-    $mail->addBCC('fdoming3@xtec.cat', 'Fco. Javier Domínguez');
-    $mail->addBCC('albertmasa2@gmail.com', 'Albert Mañosa');
-} catch (Exception $e) {
-    echo mailer_error($mail, $e);
-    return;
-}
-
-$mail->Subject = 'Nou missatge de contacte';
-
-$body = file_get_contents(ROOT . '/../src/View/email-template.php');
-
-$vars = [
-    'from' => $_POST['nom'],
-    'email' => $_POST['email'],
-    'date' => date('Y-m-d H:i'),
-    'date-format' => strftime('%A, %e %B %Y · %H:%M'),
-    'message' => $_POST['missatge']
-];
-
-foreach ($vars as $var => $value) {
-    $body = str_replace('{' . $var . '}', $value, $body);
-}
-
-try {
-    $mail->msgHTML($body, __DIR__);
-} catch (Exception $e) {
-    echo mailer_error($mail, $e);
-    return;
-}
-
-$mail->AltBody = $_POST['missatge'];
-
-try {
-    if (!$mail->send()) {
-        echo mailer_error($mail);
-        return;
-    }
-} catch (Exception $e) {
-    echo mailer_error($mail, $e);
-    return;
-}
+send_mail(
+    $_POST['email'],
+    $_POST['nom'],
+    $subject = 'Nou missatge de contacte',
+    $message = $_POST['missatge'],
+    $bccs = [
+        // ['address' => 'fdoming3@xtec.cat', 'name' => 'Fco. Javier Domínguez'],
+        ['address' => 'albertmasa2@gmail.com', 'name' => 'Albert Mañosa'],
+    ],
+    $template = 'contact',
+    $error_location = $location,
+);
 
 session_destroy();
-header('Location: https://rcdeescolasantcugat.com/contacte/?res=ok');
+header("Location: $location?res=ok");
