74bc8348e918da2106cd496171cba254b5e938c5
albertms10 <albertmasa2@gmail.com>
2021-02-17 11:22:06 +0100

    fix(contact-form-validation): change querySelector reference to current form

--- web/assets/js/contact-form-validation.js
+++ web/assets/js/contact-form-validation.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", () => {
-    const forms = document.getElementsByClassName("needs-validation");
+    const forms = document.querySelectorAll("form.needs-validation");
 
     Array.prototype.filter.call(forms, (form) => {
         form.addEventListener("submit", (event) => {
@@ -7,7 +7,7 @@ window.addEventListener("load", () => {
                 event.preventDefault();
                 event.stopPropagation();
             } else {
-                const submitButton = document.querySelector("button[type=\"submit\"]");
+                const submitButton = form.querySelector("button[type=\"submit\"]");
                 submitButton.innerHTML = `<span class="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"></span> Enviant`;
                 submitButton.disabled = true;
             }
