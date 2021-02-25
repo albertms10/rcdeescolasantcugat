fff9b9fade3f0872e5be51c7064b6a8ee6364810
albertms10 <albertmasa2@gmail.com>
2021-02-25 13:49:07 +0100

    refactor(index): rearrange svg-icon-container CSS styles

--- web/index.php
+++ web/index.php
@@ -157,9 +157,11 @@ include ROOT . '/../src/Utils/lang-init.php';
                         <div class="mt-5">
                             <?php if (isset($slogan->fa_icon)): ?>
                                 <i class="fas fa-4x <?= $slogan->fa_icon ?> text-primary mb-4"></i>
-                            <?php elseif (isset($slogan->icon_filename)):
-                                echo file_get_contents($slogan->icon_filename);
-                            endif ?>
+                            <?php elseif (isset($slogan->icon_filename)): ?>
+                                <div class="svg-icon-container small blue">
+                                    <?= file_get_contents($slogan->icon_filename) ?>
+                                </div>
+                            <?php endif ?>
                             <h3 class="h4 mb-2"><?= $slogan->title ?></h3>
                             <p class="text-muted mb-0"><?= $slogan->description ?></p>
                         </div>
