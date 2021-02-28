33504fdd05c31d0e4285a43db3a23f319d3a3b91
albertms10 <albertmasa2@gmail.com>
2021-02-28 22:49:36 +0100

    refactor(assets/js): rewrite forEach call on querySelector node list

--- web/assets/js/contact-form-validation.js
+++ web/assets/js/contact-form-validation.js
@@ -1,18 +1,17 @@
 window.addEventListener("load", () => {
-    const forms = document.querySelectorAll("form.needs-validation");
+    document.querySelectorAll("form.needs-validation")
+            .forEach((form) => {
+                form.addEventListener("submit", (event) => {
+                    if (!form.checkValidity()) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    } else {
+                        const submitButton = form.querySelector(`button[type="submit"]`);
+                        submitButton.innerHTML = `<span class="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"></span> Enviant`;
+                        submitButton.disabled = true;
+                    }
 
-    Array.prototype.filter.call(forms, (form) => {
-        form.addEventListener("submit", (event) => {
-            if (!form.checkValidity()) {
-                event.preventDefault();
-                event.stopPropagation();
-            } else {
-                const submitButton = form.querySelector(`button[type="submit"]`);
-                submitButton.innerHTML = `<span class="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"></span> Enviant`;
-                submitButton.disabled = true;
-            }
-
-            form.classList.add("was-validated");
-        }, false);
-    });
+                    form.classList.add("was-validated");
+                }, false);
+            });
 }, false);
--- web/assets/js/scroll.js
+++ web/assets/js/scroll.js
@@ -1,28 +1,27 @@
-const scrollTriggers = document.querySelectorAll(`a.js-scroll-trigger[href*="#"]:not([href="#"])`);
+document.querySelectorAll(`a.js-scroll-trigger[href*="#"]:not([href="#"])`)
+        .forEach((scrollTrigger) => {
+                scrollTrigger.addEventListener("click", function () {
+                    if (
+                        location.pathname.replace(/^\//, "") !== this.pathname.replace(/^\//, "")
+                        && location.hostname !== this.hostname
+                    ) return;
 
-Array.prototype.forEach.call(scrollTriggers, (scrollTrigger) => {
-        scrollTrigger.addEventListener("click", function () {
-            if (
-                location.pathname.replace(/^\//, "") !== this.pathname.replace(/^\//, "")
-                && location.hostname !== this.hostname
-            ) return;
+                    let target = $(this.hash);
+                    if (!target.length) target = $(`[name=${this.hash.slice(1)}]`);
 
-            let target = $(this.hash);
-            if (!target.length) target = $(`[name=${this.hash.slice(1)}]`);
+                    const offset = $(this).data("offset");
+                    const scrollAmount = target.offset().top - (offset || 110);
 
-            const offset = $(this).data("offset");
-            const scrollAmount = target.offset().top - (offset || 110);
+                    if (!target.length) return;
 
-            if (!target.length) return;
+                    $("html, body").animate({
+                        scrollTop: scrollAmount > 0 ? scrollAmount : 0
+                    }, 1000, "easeInOutExpo");
 
-            $("html, body").animate({
-                scrollTop: scrollAmount > 0 ? scrollAmount : 0
-            }, 1000, "easeInOutExpo");
-
-            return false;
-        });
-    }
-);
+                    return false;
+                });
+            }
+        );
 
 if (typeof BSN !== "undefined")
     new BSN.ScrollSpy(document.querySelector("body"), {
