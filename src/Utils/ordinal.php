d1acaec2e4de79dea94d72b64c85c92775a4a01f
albertms10 <albertmasa2@gmail.com>
2021-03-02 16:54:01 +0100

    feat(ordinal): add ordinal localization functions (ca, en, es)

--- src/Utils/ordinal.php
+++ src/Utils/ordinal.php
@@ -1,9 +1,9 @@
 <?php
 
-function ordinal(int $num, string $genere = 'fem'): string
+function ordinal_ca(int $num, bool $is_fem = true): string
 {
     $sufix = match (true) {
-        $genere === 'fem' => 'a',
+        $is_fem => 'a',
         ($num === 1) || ($num === 3) => 'r',
         $num === 2 => 'n',
         $num === 4 => 't',
@@ -11,4 +11,36 @@ function ordinal(int $num, string $genere = 'fem'): string
     };
 
     return $num . $sufix;
+}
+
+function ordinal_en(int $num): string
+{
+    $sufix = match (substr($num, -1)) {
+        '1' => 'st',
+        '2' => 'nd',
+        '3' => 'rd',
+        default => 'th',
+    };
+
+    return $num . $sufix;
+}
+
+function ordinal_es(int $num, bool $is_fem = true): string
+{
+    $sufix = match (true) {
+        $is_fem => '.ª',
+        default => '.º',
+    };
+
+    return $num . $sufix;
+}
+
+function ordinal(int $num, bool $is_fem = true, ?string $locale = null): string
+{
+    return match ($locale) {
+        'ca' => ordinal_ca($num, $is_fem),
+        'en' => ordinal_en($num),
+        'es' => ordinal_es($num, $is_fem),
+        default => $num,
+    };
 }
