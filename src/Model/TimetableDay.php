da9809ca6cab1c0f829d151744b1975ce9f80f40
albertms10 <albertmasa2@gmail.com>
2021-03-07 18:21:21 +0100

    feat(Model): add TimetableDay class

---
+++ src/Model/TimetableDay.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace RCDE;
+
+class TimetableDay
+{
+    public function __construct(
+        public int $weekday,
+        public string $startTime,
+        public ?string $endTime,
+    )
+    {
+    }
+
+    public function getTextualWeekday(): string
+    {
+        return utf8_encode(strftime('%A', 1609714800 + (86400 * $this->weekday)));
+    }
+
+    public function getTimeRange(): string
+    {
+        return $this->startTime . (isset($this->endTime) ? "–$this->endTime" : '');
+    }
+}
