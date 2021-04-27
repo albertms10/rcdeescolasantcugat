ba8bff44c48d33c4d74a9b388e389c94c16f9045
Albert Mañosa <albertmasa2@gmail.com>
2021-04-27 23:09:30 +0200

    feat(Translation): add Privacy-related files

---
+++ src/Translation/Privacy.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace RCDE\Translation;
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
+
+final class Privacy extends Translator
+{
+    public function __construct(?string $lang = null)
+    {
+        parent::__construct($lang);
+    }
+}
