f763f684c41b632ea0314a9ee530ffb6332bc3c2
Albert Mañosa <albertmasa2@gmail.com>
2021-05-16 00:08:21 +0200

    fix(Model): change Social class definition with classname member

--- src/Model/Social.php
+++ src/Model/Social.php
@@ -5,10 +5,9 @@ namespace RCDE\Model;
 class Social
 {
     public function __construct(
-        public string $icon,
         public string $title,
         public string $link,
-        public string $color,
+        public string $classname,
     )
     {
     }
